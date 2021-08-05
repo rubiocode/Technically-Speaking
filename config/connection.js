671163a65f9ed201038554824312666605e1e86d
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-04 22:32:23 -0700

    created env file and set up environmental variables, gitignore created, connection js wth sequelize code added

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+//requiring dependency
+const sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+//Using JAWSDB_URL to host database (cannot use local storage in our computer).
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else{
+    sequelize = new Sequelize(
+
+        //using .env to connect our environmental variables
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port:3306
+        }
+    );
+}
+
+//exporting sequelize
+module.exports=sequelize;
