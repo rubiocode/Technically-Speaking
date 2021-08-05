3a9ca0db6e95102da03cd3476852d9d2ff5a0190
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-05 01:14:31 -0700

    model's index.js file created, model exported, connections created among schemas

---
+++ models/index.js
@@ -0,0 +1,40 @@
+//requirig all schema files 
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+//linking all schemas (creating associations among all schemas)
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: CASCADE,
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: CASCADE,
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: CASCADE,
+});
+
+//exporting all schema files
+module.exports ={
+    User,
+    Post,
+    Comment,
+};
