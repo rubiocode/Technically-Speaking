805aa1a10378fcec65c68460069e6f1881aa2c71
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-04 23:55:11 -0700

    created Post schema, username might need revisiting after user schema created

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+//Extracting model from Sequelize and creating connection to config folder
+const {Model, DataTypes} = require('sequelize');
+const sequelize= require('../config/connection');
+
+//Create a new Sequelize model for posts
+class Post extends Model {}
+
+//defining fields/columns on model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INT,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            // using text per sequelize documentation it had unlimited length text column vs string has 255 default length
+            type: DataTypes.TEXT,
+            allowNull:false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        timestamp: true,
+        freezeTableName: true,
+        underscore: true,
+        modelName:'post'
+    }
+);
+
+module.exports =Post;
