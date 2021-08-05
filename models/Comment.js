18ef8844f3ac1d4eed417306e8fa2c0f83711354
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-05 00:58:58 -0700

    Created comment model schema, linked user_id and post_id to comment

---
+++ models/Comment.js
@@ -0,0 +1,54 @@
+//Extracting model from Sequelize and creating connection to config folder
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//Create a new sequilize model for Comment
+class Comment extends Model { };
+
+//defining fields/columns on model
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+
+        //linking user to comment
+        user_id: {
+            type: DataTypes.INT,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+
+        //linking post to comment
+        post_id: {
+            type: DataTypes.INT,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        },
+        comment_content: {
+            //want to limit the amount of characters string has max 255
+            dataType: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamp: true,
+        freezeTableName: true,
+        underscore: true,
+        modelName: 'comment',
+    }
+);
+
+//exporting comment model
+module.exports = Comment;
