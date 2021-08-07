83f7111d71aa3c3d4634c37fadb2ff000fb09d86
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-07 15:44:18 -0700

    main handlebar created

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,6 +4,9 @@ const router = require('express').Router();
 //deconstructing comment from model's folder 
 const { User, Comment, Post } = require('../../models');
 
+//require utils auth file
+const withAuth = require('../../utils/auth');
+
 //Comment logic: create new comment, update, delete and retrieve comments.
 
 // Get all comments and the blog and username they are attached to
@@ -29,5 +32,81 @@ router.get('/', async (req, res) => {
         res.status(500).json(e);
     }
 });
+
+//Create new comment
+router.post('/', async (req, res) => {
+    try {
+
+        if (req.session) {
+            const commentData = await Comment.create({
+                user_id: req.session.user_id,
+                post_id: req.body.post_id,
+                comment_content: req.body.comment_content,
+            });
+            res.status(200).json(commentData);
+        }
+        res.status(404).json({ message: 'Something went wrong!' });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+});
+
+
+//update existing comment
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        if (req.session) {
+            const updateComment = await Comment.update(
+                {
+                    user_id: req.session.user_id,
+                    post_id: req.body.post_id,
+                    comment_content: req.body.comment_content,
+                },
+                {
+                    where: {
+                        id: req.params.id,
+                    },
+                });
+                if (!updateComment[0]) {
+                    res.status(404).json({ message: 'No comment found with this id!' });
+                    return;
+                }
+                res.status(200).json(updateComment);
+        }
+        
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+
+    }
+});
+
+//Delete comment 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        if(req.session){
+            const deleteCommment = await Comment.destroy({
+                where: {
+                    id: req.params.id,
+                }
+            });
+            if (!deleteComment) {
+                res.status(404).json({ message: 'No comment found with that id' });
+                return;
+            }
+            res.status(200).json(deleteComment);
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+});
+
+
+
+
+
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,75 @@
 // getting our router stored in variable
-const router= require('express').Router();
+const router = require('express').Router();
+
+//requiring connection file
+const sequelize = require('../config/connection')
 
 //testing if seeds renders on page 
 const { User, Post, Comment } = require('../models');
 
+//homepage logic: render all posts and comments (with respective user), route to log in or sign up 
+
 router.get('/', async (req, res) => {
     try {
-    const userData = await User.findAll({});
-    const users=userData.map(user => user.get({plain:true}));
-
-    console.log(users);
-    res.render('homepage', {users});
+        const userData = await User.findAll(
+            {
+                attributes: ['id', 'title', 'post_content'],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username', 'twitter', 'github']
+                    },
+                    {
+                        model: Comment,
+                        attributes: ['id', 'user_id', 'post_id', 'comment_content']
+                    },
+                ],
+            },
+        );
+        const users = userData.map(user => user.get({ plain: true }));
+        console.log(users);
+        res.render('homepage', { users, loggedIn: req.session.loggedIn });
     } catch (e) {
-    res.status(400).json(e);
+        res.status(400).json(e);
     }
 });
+
+
+//Signing up in the homepage 
+
+router.get('/signup', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.redirect('/');
+            return
+        }
+        res.status(200).render('signup');
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+});
+
+//Loging in the homepage
+router.get('/login', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.redirect('/');
+            return
+        }
+        res.status(200).render('login');
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+});
+
+
+//grab single blog to view in the homepage
+
+
+
+
+
 
 module.exports = router;
