be51301ec203edb80bb1a096c6f20610d054ef52
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-10 00:50:28 -0700

    create post and submit post working properly, syntax error in add-post js fixed

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -2,8 +2,8 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
-
+    const post_content = document.querySelector('textarea[name="post-content"]').value;
+    
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
