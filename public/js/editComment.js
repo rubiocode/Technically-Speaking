2090dae130f7c170e276bc57fbaa988f1e021723
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-08-10 15:14:10 -0700

    debugging created at, comments rendering on page and being posted

---
+++ public/js/editComment.js
@@ -0,0 +1,26 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const comment_content = document.querySelector('textarea[name="comment-body"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/comments/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            comment_content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#update').addEventListener('click', editFormHandler);
